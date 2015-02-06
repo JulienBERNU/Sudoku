8615052fc3d5c5035c86d316dabd9b0e2e4fdf80
Julien <Julien@physics0114.anu.edu.au>
2015-02-06 14:44:19 +1100

    Initial Commit

---
+++ Sudoku/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sudoku
+//
+//  Created by Julien on 6/02/2015.
+//  Copyright (c) 2015 Julien. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
